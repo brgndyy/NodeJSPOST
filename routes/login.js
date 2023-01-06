54301491adfad7d2ac35590ff784c8fb18bc8a5f
TAEHEON <brgndy96@gmail.com>
2023-01-06 10:49:42 +0900

    Add files via upload

--- routes/login.js
+++ routes/login.js
@@ -7,7 +7,9 @@ router.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../views/login.html"));
 });
 
+// /login 이 메인라우터이고 이 안에서 /add이므로 form 태그 내에서 /login/add를 해주어야함.
 router.post("/add", (req, res) => {
+  console.log(req.body);
   res.send("전송 완료");
 });
 
